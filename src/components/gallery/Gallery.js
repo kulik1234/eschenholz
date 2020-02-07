a5d72cd34205c1063addef9cd88fa40c39ca1c06
A B <bogdan_123456767854@interia.pl>
2020-02-07 13:23:11 +0100

    new components

---
+++ src/components/gallery/Gallery.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class Gallery extends React.Component {
+
+
+
+
+    render() {
+      return (
+        <div>
+         To jest galeria
+        </div>
+      );
+    }
+  }
+  
+  export default Gallery;
+
+
