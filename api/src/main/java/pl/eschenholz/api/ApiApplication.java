df9fc51e6844285d8944c76d457d47976089a030
A B <bogdan_123456767854@interia.pl>
2020-02-10 12:05:44 +0100

    controller upgrade next try

--- api/src/main/java/pl/eschenholz/api/ApiApplication.java
+++ api/src/main/java/pl/eschenholz/api/ApiApplication.java
@@ -2,6 +2,7 @@ package pl.eschenholz.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
 
 import java.io.*;
 import java.nio.Buffer;
@@ -11,47 +12,8 @@ import java.util.ArrayList;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiApplication.class, args);
-	}
-	private static void preapreEndpoints(){
-		ArrayList<String> list = new ArrayList<>();
-		try {
-			FileReader filer = new FileReader("C:\\Users\\Bogdan\\React\\eschenholz\\api\\src\\main\\resources\\application.properties");
-			BufferedReader reader = new BufferedReader(filer);
-			reader.lines().forEach(line -> {
-				list.add(line);
-						System.out.println(line);
-					}
-			);
-			System.out.println("To jest koniec odzczytu czas na zapis");
-			filer.close();
-			reader.close();
-			FileWriter filew = new FileWriter("C:\\Users\\Bogdan\\React\\eschenholz\\api\\src\\main\\resources\\application.properties");
-			BufferedWriter writer = new BufferedWriter(filew);
-			list.add("api.endpoint=contact-form");
-			list.stream().forEach(line->{
-				System.out.println(line);
-				char[] a = line.toCharArray();
-				for(char ch : a) {
-					try {
-						writer.append(ch);
-						writer.flush();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-				try {
-					writer.newLine();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			});
-			filew.close();
-			writer.close();
-		}
-		catch (IOException e){
-			System.out.println(e);
-		}
 	}
 
 
