f8326c5925e99427fb7e4aff665fe6d304ff6c7c
A B <bogdan_123456767854@interia.pl>
2020-02-11 12:08:14 +0100

    added google map

---
+++ src/components/contact/modules/GoogleMap.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import Style from './css/GoogleMapStyles.module.css';
+
+const mapStyles = {
+    width: '1000px',
+    height: '600px'
+  };
+  
+
+export class GoogleMap extends React.Component {
+    state = {
+        showingInfoWindow: false,  
+        activeMarker: {},         
+        selectedPlace: {}          
+      };
+
+      onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+
+    render() {
+      return (
+          <div className={Style.main}>
+              <div>
+                  <p>Zobacz gdzie znajduje się siedziba firmy</p>
+              </div>
+        <div id="google-map" Style="position:relative;overflow:hidden;height:600px;width:1000px;margin:10px auto 0px;">
+            <Map
+                  google={this.props.google}
+                  zoom={16}
+                  style={mapStyles}
+                  initialCenter={{
+                      lat: 49.756832,
+                      lng: 20.414998
+                  }}
+              >
+              <Marker
+          onClick={this.onMarkerClick}
+          name={'Firma stolarska "Eschenholz"'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+            <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow></Map>
+        </div>
+        </div>
+      );
+    }
+  }
+  
+  
+export default GoogleApiWrapper({ apiKey: 'AIzaSyC5JhuN4hAknPU3RX-DMQ6dmfkZXyUcY00'})(GoogleMap);
